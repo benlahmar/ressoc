89d263e2494c98ba864606698d4f68a732d52c50
h.benlahmer <h.benlahmer@gmail.com>
2022-05-23 22:13:34 +0000

    init

---
+++ app/Http/Controllers/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Post extends Controller
+{
+    //
+   public function get()
+    {
+        return "ggg";
+    }
+
+    public function get2()
+    {
+        return "hhhh";
+    }
+}
